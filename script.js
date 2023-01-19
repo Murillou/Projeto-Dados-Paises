d5391b583799eb7823c788b3361592c3827f8f49
Murillo Vinícius <Murillovinicius13@hotmail.com>
2023-01-18 22:46:16 -0300

    Atualizando o código
    Input de busca funcionando perfeitamente

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
 const count = document.querySelector('h3')
 count.innerHTML = `Número total de países é de: ${paises.length}`
 
-let container = document.createElement('div')
+const searchInput = document.querySelector('#search')
+const tableBody = document.querySelector('#countries-table-body')
 
-let table = document.createElement('table')
-
-let contadorPais = 0
-
-while(contadorPais < paises.length){
-    let linha = document.createElement('tr')
-
-    for(let i = 0; i < 6; i++){
-        const dados = document.createElement('td')
-        dados.textContent = paises[contadorPais]
-
-        
-        linha.appendChild(dados)
-        contadorPais++
+function populateTable(countries){
+    tableBody.innerHTML = ''
+    let row = document.createElement('tr')
+    for(let i = 0; i < countries.length; i++){
+        if (i % 6 == 0 && i !== 0) {
+            tableBody.appendChild(row)
+            row = document.createElement('tr')
+        }
+        const nameCell = document.createElement('td')
+        nameCell.textContent = countries[i]
+        row.appendChild(nameCell)
     }
-    table.appendChild(linha)
+    tableBody.appendChild(row)
 }
 
+const btn = document.querySelector("#btn")
+
+btn.addEventListener('click', () => {
 
 
-container.appendChild(table)
+function filterCountries(){
+    const searchTerm = searchInput.value.toLowerCase()
+    const filteredCountries = paises.filter(country =>
+    country.toLowerCase().startsWith(searchTerm))
+    populateTable(filteredCountries)
+    const counts = document.querySelector('h4')
+    counts.innerHTML = `Número total de países que começam com <strong id = "letter">${searchTerm}</strong> é de: 
+    <span id = "number">${filteredCountries.length}</span>`
+}
 
-document.body.appendChild(container)
+searchInput.addEventListener('input', filterCountries)
+populateTable(paises)
+
+})
+
+const btnTwo = document.querySelector("#btnTwo")
+
+btnTwo.addEventListener('click', () => {
+
+
+function filterCountries(){
+    const searchTerm = searchInput.value.toLowerCase()
+    const filteredCountries = paises.filter(country =>
+    country.toLowerCase().includes(searchTerm))
+    populateTable(filteredCountries)
+    const counts = document.querySelector('h4')
+    counts.innerHTML = `Número total de países que contém <strong id = "letter">${searchTerm}</strong> é de: 
+    <span id = "number">${filteredCountries.length}</span>`
+}
+
+searchInput.addEventListener('input', filterCountries)
+populateTable(paises)
+
+})
+
+populateTable(paises)
+
